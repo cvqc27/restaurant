707d33ecf063c39b8057672ec71e78f47da557e8
vidal quispe callañaupa <69360965+cvqc27@users.noreply.github.com>
2023-10-05 16:24:18 -0500

    Update app.js
    mi primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let left = document.getElementsByClassName('bi-caret-left-fill')[0];
+let right = document.getElementsByClassName('bi-caret-right')[0];
+let cards = document.getElementsByClassName('cards')[0];
+
+left.addEventListener('click',()=>{
+    cards.scrollLeft -= 140;
+})
+
+right.addEventListener('click',()=>{
+    cards.scrollLeft += 140;
+})
+
+let poster = document.getElementById('poster');
+let title = document.getElementById("title");
+let price_cont = document.getElementById("price_cont");
+
+Array.from(document.getElementsByClassName('card')).forEach((ele, i) =>(
+    //console.log
+    ele.addEventListener('click', ()=>{
+        poster.src =ele.getElementsByTagName('img')[0].scr;
+    })
+))
+
